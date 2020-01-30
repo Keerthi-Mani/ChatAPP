4e2f6f76a6bb85f541e01939dd1bf68e957b1697
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-30 09:19:27 -0500

    emit messages

--- server.js
+++ server.js
@@ -8,5 +8,36 @@ mongo.connect(url, function(err, db) {
   if (err) {
     throw err;
   }
+
   console.log("Mongodb is connected .....");
+
+  // Connect to Socket.io
+  client.on("connection", function(socket) {
+    let chat = db.collection("chats");
+
+    //Create a function to send status
+    sendStatus = function(status) {
+      socket.emit("status", status);
+    };
+
+    //Get chats from mongo collection
+    chat
+      .find()
+      .limit(100)
+      .sort({ _id: 1 })
+      .toArray(function(err, res) {
+        if (err) {
+          throw err;
+        }
+
+        //Emit the messages to the client
+        socket.emit("output", res);
+      });
+
+    //Handle input events
+    socket.on("input", function(data) {
+      let name = data.name;
+      let message = data.message;
+    });
+  });
 });
