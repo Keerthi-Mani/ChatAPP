8354c84193833e9cc16bb5f4ef5c7865a7aeee40
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-30 09:53:29 -0500

    get element by id

---
+++ chat.js
@@ -0,0 +1,10 @@
+(function() {
+  //Get Elements
+  var userName = document.getElementBy("username");
+  var textArea = document.getElementBy("textarea");
+  var messages = document.getElementBy("messages");
+  var status = document.getElementBy("status");
+  var clearBtn = document.getElementBy("clear");
+});
+
+//Handle input
