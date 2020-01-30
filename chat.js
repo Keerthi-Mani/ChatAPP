63cda54d9d56020b57bb03118f631c999fa3b03d
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-30 10:12:56 -0500

    check for connection

--- chat.js
+++ chat.js
@@ -1,10 +1,10 @@
 (function() {
   //Get Elements
-  var userName = document.getElementBy("username");
-  var textArea = document.getElementBy("textarea");
-  var messages = document.getElementBy("messages");
-  var status = document.getElementBy("status");
-  var clearBtn = document.getElementBy("clear");
+  var userName = document.getElementById("username");
+  var textArea = document.getElementById("textarea");
+  var messages = document.getElementById("messages");
+  var status = document.getElementById("status");
+  var clearBtn = document.getElementById("clear");
 
   //Status
   var statusDefault = status.textContent;
@@ -20,20 +20,41 @@
     }
   };
 
-  //Handle input
-  textArea.addEventListener("keyDown", function(event) {
-    event.preventDefault();
-    if (event.which === 13 && event.shiftKey == false) {
-      //Emit to server input
-      socket.emit("input", {
-        name: userName.value,
-        message: textArea.value
-      });
-    }
-  });
+  // Connect to socket.io
+  var socket = io.connect("http://127.0.0.1:4000");
 
-  //Handle Chat clear
-  clearBtn.addEventListener("click", function() {
-    socket.emit("clear");
-  });
+  //Check for connection
+  if (socket !== undefined) {
+    console.log("Connected to socket...");
+
+    //Handle output
+    socket.on("output", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        //Build out message div
+        var message = document.createElement("div");
+        message.setAttribute("class", "chat-message");
+        message.textContent = data[i].name + ": " + data[i].message;
+        messages.appendChild(message);
+        messages.insertBefore(message, messages.firstChild);
+      }
+    });
+
+    //Handle input
+    textArea.addEventListener("keyDown", function(event) {
+      event.preventDefault();
+      if (event.which === 13 && event.shiftKey == false) {
+        //Emit to server input
+        socket.emit("input", {
+          name: userName.value,
+          message: textArea.value
+        });
+      }
+    });
+
+    //Handle Chat clear
+    clearBtn.addEventListener("click", function() {
+      socket.emit("clear");
+    });
+  }
 })();
